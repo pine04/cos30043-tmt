6ccdd507da66ad9b4ef5f3e4a7ce4bc92531783a
gnut <tunggnut2004@gmail.com>
2024-07-15 11:37:03 +0700

    Clean up backend slightly

--- 6.2HD/tmt-backend/src/services/pool.js
+++ 6.2HD/tmt-backend/src/services/pool.js
@@ -1,13 +1,11 @@
 const mysql = require("mysql2/promise");
 
-const opts = {
+const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_DATABASE || "tmt"
-}
-
-const pool = mysql.createPool(opts);
+});
 
 module.exports = pool;
