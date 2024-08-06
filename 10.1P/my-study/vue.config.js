74c5d4532c22d5d138d116fd065cd7b54beedcdb
gnut <tunggnut2004@gmail.com>
2024-08-07 00:42:25 +0700

    Removed redundant files uploaded by mistake.

--- 10.1P/my-study/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: "10p.lvh.me"
-  }
-})
--- 9.2C/store/store.js
+++
@@ -1,9 +0,0 @@
-const store = Vue.reactive({
-    isAuthenticated: false,
-    authenticate() {
-        this.isAuthenticated = true;
-    },
-    unauthenticate() {
-        this.isAuthenticated = false;
-    }
-});
